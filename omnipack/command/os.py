61d5bcea267d01eb3460075da09b108646b3a3a0
Frank Lee <somerlee.9@gmail.com>
2020-06-17 22:46:39 +0800

    [UPDATE] update cli

--- omnipack/command/os.py
+++ omnipack/command/os.py
@@ -5,13 +5,13 @@ import omnipack
 
 
 @click.command(help='Kill process with the same command')
-@click.argument('cmd')
+@click.argument('cmd', type=str)
 def kill_multiprocess(cmd):
     omnipack.kill_multiprocess(cmd)
 
 
 @click.command(help='Kill process with pid')
-@click.argument('pid')
+@click.argument('pid', type=int)
 def kill_process(pid):
     omnipack.kill_process(int(pid))
 
@@ -19,15 +19,15 @@ def kill_process(pid):
 # -------- network -------- #
 
 @click.command(help='Kill port connection')
-@click.argument('port')
+@click.argument('port', type=int)
 def kill_port(port):
-    omnipack.kill_port(int(port))
+    omnipack.kill_port(port)
 
 
 @click.command(help='Check port availability')
-@click.argument('port')
+@click.argument('port', type=int)
 def is_port_available(port):
-    avai = omnipack.is_port_available(int(port))
+    avai = omnipack.is_port_available(port)
     print(avai)
 
 
